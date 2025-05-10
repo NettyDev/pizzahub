b45f27a971b94a7840d39ed058f21b77a206b922
Grzegorz Szwyngiel <grzegorz.szwyngiel@gmail.com>
2025-05-10 10:54:57 +0200

    menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,11 +1,48 @@
 import { Switch } from "@/components/ui/switch";
 import MenuComp from "@/components/Menu";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Menu() {
   return (
     <>
       <MenuComp />
-      <h1>Menu</h1>
+      <div className="w-auto flex justify-center h-50 items-center bg-[url(/menu.jpg)] bg-cover">
+        <h1 className="flex text-6xl text-white ">Menu</h1>
+      </div>
+      <div className="w-full flex justify-center">
+        <div className="w-200 flex flex-wrap">
+          {[
+            "Onuris",
+            "El-naga",
+            "Al-marcianoo",
+            "Ell-tafir",
+            "Egipcjanka",
+            "Sphinks",
+            "Faraon",
+            "Kleopatra",
+            "Tutenchamon"
+          ].map((v) => (
+            <div className="w-1/3">
+              <Card className="m-4">
+                <CardHeader>
+                  <CardTitle>{v}</CardTitle>
+                  {/* <CardDescription>test</CardDescription> */}
+                </CardHeader>
+                <CardContent>
+                  <img src="/pizza-placeholder.png" width="100%" alt="" />
+                </CardContent>
+                <CardFooter>
+                  <Button className="w-full" size="sm">
+                    Dodaj do koszyka
+                  </Button>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
